27264228cfd2b8da7418ae0e2e9cd318e8b258bb
kartik-teja <kartikteja343@gmail.com>
2024-05-31 16:24:07 +0530

    fixed bugs

--- src/contexts/Matches/initialData.ts
+++ src/contexts/Matches/initialData.ts
@@ -1,26 +1,23 @@
 import { LiveMatches } from "./types";
 
-const initialData: LiveMatches = {
+const initialData: LiveMatches = [{
+    "id": 3,
+    "name": "Titans VS Vortex Vipers at Unity Arena, Concordia",
+    "location": "Unity Arena, Concordia",
+    "sportName": "American Football",
+    "endsAt": "2023-06-08T15:25:16.142Z",
+    "isRunning": false,
+    "teams": [
+        {
+            "id": 5,
+            "name": "Titans"
+        },
+        {
+            "id": 6,
+            "name": "Vortex Vipers"
+        }
+    ]
 
-    "1": {
-        "id": 3,
-        "name": "Titans VS Vortex Vipers at Unity Arena, Concordia",
-        "location": "Unity Arena, Concordia",
-        "sportName": "American Football",
-        "endsAt": "2023-06-08T15:25:16.142Z",
-        "isRunning": false,
-        "teams": [
-            {
-                "id": 5,
-                "name": "Titans"
-            },
-            {
-                "id": 6,
-                "name": "Vortex Vipers"
-            }
-        ]
-    },
-
-}
+}]
 
 export default initialData;
--- src/contexts/News/initialData.ts
+++ src/contexts/News/initialData.ts
@@ -10,7 +10,8 @@ const initialData: News[] = [
         },
         date: "2023-08-01T12:08:33.811Z",
         summary: "A game filled with excitement, suspense, and drama, a true reflection of the spirit of the sport",
-        teams: [
+        content: "The excitement was in the air as the Table Tennis match was about to start. It was the day of the big game between Stealth Strikers and Celestial Chargers, and the fans couldn't be more excited.\n\n\n  The game started on a high note, with both teams showcasing their skills and tactics. The crowd was on their feet, cheering for their teams. The first half of the game saw Stealth Strikers taking a slight lead. But Celestial Chargers was not far behind.\n\n\n  As the second half of the game started, Celestial Chargers came back stronger. They were determined to win the game. Their strategy was working, and they managed to level the scores.\n\n\n  The last few minutes of the game were a nail-biter. Both teams had their fair share of chances, but the game ended in a draw. It was a match that was filled with excitement, suspense, and drama, a true reflection of the spirit of Table Tennis."
+        , teams: [
             {
                 id: 20,
                 name: "Stealth Strikers"
