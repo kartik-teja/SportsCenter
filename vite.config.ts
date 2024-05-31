6f07cef017af4f8e4f0c223ef918f99cc2afc2ce
kartik-teja <kartikteja343@gmail.com>
2024-05-31 17:48:31 +0530

    Production ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "spotscenter",
+        short_name: "spotscenter",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#317EFB",
+        lang: "en",
+        scope: "/"
+      }
+    })
+  ],
+  mode: 'production'
+});
